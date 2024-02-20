041ba4df81599d55e17520c189e47ed0c875916d
lcoric3 <luka.coric3@gmail.com>
2024-02-20 11:23:17 +0100

    Summary

--- film_datebase.py
+++ film_datebase.py
@@ -7,4 +7,56 @@ class FilmDateBase:
         self.conn = sqlite3.connect(db_file)
         self.cursor = self.conn.cursor()
 
-        self.cursor.execute
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS filmovi(
+                id INTEGER PRIMATY KEY AUTOINCREMENT,
+                naslov TEXT,
+                godina INTEGER,
+                redatelj TEXT
+                )
+                """)
+        self.conn.commit()
+
+    def dodaj_film(self, naslov, godina, redatelj):
+        self.cursor.execute("""
+            INSERT INTO filmovi(naslov, godina, redatelj)
+            VALUES (?, ?, ?)
+        """, (naslov, godina, redatelj))
+        self.conn.commit()
+        return self.cursor.lastrowid
+    
+    def ukloni_film(self, film_id):
+        self.cursor.execute("""
+            DELETE FROM filmovi 
+            WHERE id = ?
+        """, (film_id,))
+        self.conn.commit()
+    
+    def azuriraj_film(self, film_id, naslov, godina, redatelj):
+        self.cursor.execute("""
+            UPDATE filmovi
+            SET naslov = ?, godina = ?, redatelj = ?
+            WHERE id = ?
+        """, (naslov, godina, redatelj, film_id))
+        self.conn.commit()
+
+    def zatvori_bazu(self):
+        self.conn.close()
+                            
+
+
+
+
+              
+                            
+                            
+                            
+                            
+                           
+
+                            
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,35 +1,86 @@
 from film import Movie
 from film_api import get_movie_info, write_to_csv
+from film_datebase import FilmDateBase
 
 def main():
-    # Stvaranje objekta filma
-    film = Movie("The Shawshank Redemption", 1994, "Frank Darabont", 9.3)
+      # Stvaranje instance baze podataka
+    db = FilmDateBase("filmovi.db")
 
-    # Prikaz informacija o filmu
-    film.display_info()
+    while True:
+        # Ispis opcija
+        print("Odaberite opciju:")
+        print("1. Dodaj film")
+        print("2. Ažuriraj film")
+        print("3. Ukloni film")
+        print("4. Prikaz svih filmova")
+        print("5. Izlaz")
 
-    # Spremanje informacija o filmu u datoteku
-    Movie.save_to_file(film, "film.txt")
+        opcija = input("Unesite broj opcije: ")
 
-    # Učitavanje informacija o filmu iz datoteke
-    ucitani_film = Movie.load_from_file("film.txt")
-    ucitani_film.display_info()
+        if opcija == "1":
+            # Unos informacija o filmu
+            naslov = input("Unesite naslov filma: ")
+            godina = input("Unesite godinu filma: ")
+            redatelj = input("Unesite ime redatelja: ")
+            ocjena = input("Unesite ocjenu filma: ")
 
-    movie_info = get_movie_info(film)
-    if movie_info:
-        print("Informacije o filmu:")
-        print(f"Naslov: {movie_info['title']}")
-        print(f"Godina: {movie_info['year']}")
-        print(f"Redatelj: {movie_info['director']}")
-        print(f"Ocjena: {movie_info['rating']}")
+            # Dodavanje filma u bazu podataka
+            db.dodaj_film(naslov, godina, redatelj, ocjena)
+            print("Film je dodan u bazu podataka.")
 
-        # Pisanje u csv datoteku
-        write_to_csv(movie_info, "filmovi.csv")
-        print("Informacije su zapisane u datoteku 'filmovi.csv' ")
-    else:
-        print("Nije moguce pronaci informacije o filmu.")
+        elif opcija == "2":
+            # Unos ID-a filma za ažuriranje
+            film_id = input("Unesite ID filma koji želite ažurirati: ")
+
+            # Provjera postojanja filma u bazi podataka
+            if db.provjeri_film(film_id):
+                # Unos novih informacija o filmu
+                naslov = input("Unesite novi naslov filma: ")
+                godina = input("Unesite novu godinu filma: ")
+                redatelj = input("Unesite novo ime redatelja: ")
+                ocjena = input("Unesite novu ocjenu filma: ")
+
+                # Ažuriranje filma u bazi podataka
+                db.azuriraj_film(film_id, naslov, godina, redatelj, ocjena)
+                print("Film je ažuriran.")
+
+            else:
+                print("Film s unesenim ID-om ne postoji.")
+
+        elif opcija == "3":
+            # Unos ID-a filma za uklanjanje
+            film_id = input("Unesite ID filma koji želite ukloniti: ")
+
+            # Uklanjanje filma iz baze podataka
+            db.ukloni_film(film_id)
+            print("Film je uklonjen iz baze podataka.")
+
+        elif opcija == "4":
+            # Prikaz svih filmova iz baze podataka
+            filmovi = db.prikazi_filmove()
+
+            if filmovi:
+                print("Popis svih filmova:")
+                for film in filmovi:
+                    print(film)
+            else:
+                print("Baza podataka je prazna.")
+
+        elif opcija == "5":
+            # Zatvaranje veze s bazom podataka
+            db.zatvori_bazu()
+            break
+
+        else:
+            print("Neispravna opcija. Molimo odaberite ponovno.")
+
+
+   
+ 
 
 
  
+
+
 if __name__ == "__main__":
     main()
